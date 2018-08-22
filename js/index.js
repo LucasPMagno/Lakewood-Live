3bb96b9a9676e9fbb61c81a6f151b447b121ac87
LucasPMagno <lucaspmagno@gmail.com>
2018-08-21 23:10:38 -0400

    yey

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
+const channelID = 'UCWB1TQHxLpuEp9hcKnUX1HA';
 const API_KEY = 'AIzaSyAeFZ6hITgOOyEPaeCdbOvLYDak-RlIE18';
 const id = 'UCWB1TQHxLpuEp9hcKnUX1HA';
 const recentVideos = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&list=PLyXhNarRdOEy8Kz3VRgHsq6grb6jlE3Z6&channelId=${id}&part=snippet,id&order=date&maxResults=10`;
-const recentLWL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${API_KEY}&playlistId=PLyXhNarRdOEy8Kz3VRgHsq6grb6jlE3Z6&part=snippet,id&order=date&maxResults=1`;
+// const recentLWL2 = `https://www.googleapis.com/youtube/v3/playlistItems?key=${API_KEY}&playlistId=PLyXhNarRdOEy8Kz3VRgHsq6grb6jlE3Z6&part=snippet,id&order=date&maxResults=1`;
+const recentLWL = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&order=date&safeSearch=none&maxResults=1&channelId=${channelID}&key=${API_KEY}`
 const videoContainer = document.getElementById('video-container');
 
 $.get(recentVideos, (data) => {
     const recentItems = data.items;
-    console.log(recentItems);
+    // console.log(recentItems);
     if (recentItems) {
         let output = '<h4 class="latest-videos-title">Latest Videos</h4>';
         recentItems.forEach(item => {
@@ -28,6 +30,6 @@ $.get(recentVideos, (data) => {
 });
 
 $.get(recentLWL, (data) => {
-    console.log(data);
-    document.getElementById('latest-lwl').src=`https://www.youtube.com/embed/${data.items["0"].snippet.resourceId.videoId}?rel=0&amp;&modestbranding=1&amp;color=white&amp;&listType=playlist&amp;list=PLyXhNarRdOEy8Kz3VRgHsq6grb6jlE3Z6`;
+    // console.log(data);
+    document.getElementById('latest-lwl').src=`https://www.youtube.com/embed/${data.items["0"].id.videoId}?rel=0&amp;&modestbranding=1&amp;color=white&amp;&listType=playlist&amp;list=PLyXhNarRdOEy8Kz3VRgHsq6grb6jlE3Z6`;
 });
