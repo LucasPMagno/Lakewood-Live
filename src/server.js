f33a8cd8f0a661ff30d3ef68f12d97a4fb17e59d
LucasPMagno <lucaspmagno@gmail.com>
2018-09-08 15:05:17 -0400

    change to expressjs

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const path = require('path');
+
+const middlewares = [
+    express.static(path.join(__dirname, 'public'))
+]
+app.use(middlewares);
+
+app.use((req, res, next) => {
+    res.status(404).send('Error 404: Not Found.');
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Error 500: Server Error');
+})
+
+app.listen(8001, () => {
+    console.log('lucaspmagno running at http://localhost:8001');
+})
